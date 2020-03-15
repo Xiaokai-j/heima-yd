3a7cec4f25bc55da51b855f1d2cb626b697a7fe6
King <15176686613k@sina.com>
2020-03-15 11:11:13 +0800

    安装vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入vant组件库
+import Vant from 'vant'
+// 引入vant组件 样式
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
-
+Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 new Vue({
   router,
   store,
