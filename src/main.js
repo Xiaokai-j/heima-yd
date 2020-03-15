02275f2dec1a86e2000111b3152213cf3d4978bc
King <15176686613k@sina.com>
2020-03-15 15:22:32 +0800

    判断逻辑-跳转搜索页面

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import store from './store'
 import Vant from 'vant'
 // 引入vant组件 样式
 import 'vant/lib/index.css'
-//
+// 引入
 import 'amfe-flexible'
+// 引入less样式
+import '@/styles/index.less'
 Vue.config.productionTip = false
 Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 new Vue({
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
+  <div class="container">
       公用布局
+      <!--导航组件 采用vant的导航组件 来实现 -->
+      <!--当页面地址是/user 的时候 应该隐藏导航栏 否则不隐藏  -->
+      <!-- @事件名="逻辑" -->
+      <van-nav-bar @click-right="$router.push('/search')"  fixed title="黑马头条" right-text="搜索" v-if="$router.path!=='/user'"></van-nav-bar>
+       <!-- 二级路由容器  首页/问答/视频/我的-->
+       <!-- 判断当前的地址 如果是/user 就给一个 noTop Class -->
+       <!-- :class="{ 名称: 布尔值 }" -->
+    <div class="my-wrapper" :class="{noTop:$router.path!=='/user'}">
+      <router-view></router-view>
+    </div>
+    <!-- 标签栏组件 -->
+    <!-- route 表示开启路由模式 -->
+      <van-tabbar route>
+      <!-- router-link  to  -->
+      <!-- 点击标签栏跳转事件 to="/"-->
+      <!-- van-tabbar-item 可以当作 router-link 使用 -->
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+      <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
@@ -10,6 +31,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  position: relative;
+//   控制二级路由容器高宽
+  .my-wrapper{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop{
+      padding-top: 0;
+    }
+  }
+}
 </style>
